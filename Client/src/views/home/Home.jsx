cb3840507b798f3abfaf3b84c4a4fc7f47dc23df
ManuelGalan <mafagahe@gmail.com>
2024-02-21 16:03:21 -0500

    "Home sin filtros"

--- Client/src/views/home/Home.jsx
+++ Client/src/views/home/Home.jsx
@@ -1,21 +1,36 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { allPokemons } from "../../../Services/Pokemons/allPokemons";
 import CardsContainer from "./CardsContainer/CardsContainer";
 import NavBar from "../landing/NavBar/NavBar"
-const Home = () => { 
+import PaginationContainer from "./Pagination/Pagination";
+import SearchBar from "./SearchBar/SearchBar";
 
+const Home = () => {
+  const [currentPage, setCurrentPage] = useState(1);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(allPokemons(1))
-  }, []);
-  
-  return (
+    dispatch(allPokemons(currentPage));
+  }, [currentPage, dispatch]);
 
+  const handlePageChange = (event, page) => {
+    
+    setCurrentPage(page);
+    setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }, 10);
+  };
+
+  return (
     <div>
       <NavBar />
-      <CardsContainer />
+      <SearchBar />
+      <CardsContainer currentPage={currentPage} />
+      <PaginationContainer onPageChange={handlePageChange} />
     </div>
   );
 };
---
+++ Client/src/views/home/SearchBar/SearchBar.jsx
@@ -0,0 +1,111 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import Filter from './Filter/Filter';
+import SearchClose from "@mui/icons-material/Close";
+import OutlinedAlerts from '../Alert/Alert'
+
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { pokemonsByName } from '../../../../Services/Pokemons/pokemonsByName';
+import { resetSearch } from '../../../redux/features/pokemonSlice';
+
+const Search = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  flex: '0 0 auto',
+  padding: theme.spacing(0, 1),
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  flex: '1 1 auto',
+  padding: theme.spacing(1, 1, 1, 0),
+  paddingLeft: `calc(1em + ${theme.spacing(1)})`,
+  transition: theme.transitions.create('width'),
+  [theme.breakpoints.up('sm')]: {
+    width: '20ch',
+    '&:focus': {
+      width: '25ch',
+    },
+  },
+}));
+
+const CloseWrapper = styled('div')(({ theme }) => ({
+  flex: '0 0 auto',
+  padding: theme.spacing(0, 1),
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+export default function SearchBar() {
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
+  
+  const handleSubmit = () => {
+    if (name.trim() !== "") {
+      dispatch(pokemonsByName(name.trim()));
+    } else {
+      dispatch(resetSearch());
+    }
+  };
+
+  const handleReset = () => {
+    setName("");
+    dispatch(resetSearch());
+  };
+
+  useEffect(() => {
+    handleSubmit();
+  }, [name, dispatch]);
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Filter />
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            {name.length > 0 && (
+              <CloseWrapper>
+                <SearchClose onClick={handleReset} />
+              </CloseWrapper>
+            )}
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
